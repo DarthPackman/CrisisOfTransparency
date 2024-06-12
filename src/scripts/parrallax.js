bddcc6ba6d181f99d37736e15082e51d8db4ed23
DarthPackman <packman150@hotmail.com>
2024-06-12 01:09:36 -0700

    restart

--- src/scripts/parrallax.js
+++ src/scripts/parrallax.js
@@ -1,15 +1,9 @@
-
-document.addEventListener('scroll', function () {
-  const sections = document.querySelectorAll('.section');
-
-  sections.forEach(section => {
-    const layers = section.querySelectorAll('.parallax__layer');
-    const top = window.pageYOffset - section.offsetTop;
-
-    layers.forEach(layer => {
-      const speed = layer.getAttribute('data-speed');
-      const yPos = -(top * speed);
-      layer.style.transform = `translateY(${yPos}px)`;
-    });
+document.addEventListener('scroll', () => {
+  const layers = document.querySelectorAll('.parallax__layer');
+  layers.forEach(layer => {
+    const speed = layer.getAttribute('data-speed');
+    const yOffset = window.scrollY * speed;
+    layer.style.transform = `translateY(${yOffset}px)`;
   });
 });
+
