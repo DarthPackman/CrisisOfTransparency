d6b567207064b03ca210828909c7f135bbea8231
DarthPackman <packman150@hotmail.com>
2024-06-17 22:28:38 -0700

    styles

--- src/scripts/parrallax.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-  const layers = document.querySelectorAll('.parallax__layer');
-  layers.forEach(layer => {
-    const speed = layer.getAttribute('data-speed');
-    const yOffset = window.scrollY * speed;
-    layer.style.transform = `translateY(${yOffset}px)`;
-  });
-});
-
