c469792bf43b5c9a1858212e06b6308036652a4b
DarthPackman <packman150@hotmail.com>
2024-07-30 21:23:46 -0700

    font size

--- src/layouts/BarGraph.jsx
+++ src/layouts/BarGraph.jsx
@@ -69,6 +69,9 @@ const BarGraph = () => {
       legend: {
         labels: {
           color: 'white',
+          font: {
+            size: 18, // Change this to the desired font size
+          },
         },
       },
       title: {
