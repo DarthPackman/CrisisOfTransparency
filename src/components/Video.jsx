de66b869630e795a5eb38cdcb5c83fa66dcbaad8
DarthPackman <packman150@hotmail.com>
2024-08-02 14:39:02 -0700

    adjust video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,11 @@
 import './Video.css'
 
-const Video = ({ videoPath, alignment }) => {
+const Video = ({ videoPath, videoPath2, alignment }) => {
     return (
         <div className='VidContainer' >
             <video autoPlay className={`video ${alignment}`} muted loop>
-                <source src={videoPath} type="video/mp4" />
+                <source src={videoPath} type="video/webm" />
+                <source src={videoPath2} type="video/mp4" />
             </video>
         </div>
     )
