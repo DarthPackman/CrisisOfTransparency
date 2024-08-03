2f2ed2d76715c7ef4234d3f625bca58d741c8bf2
DarthPackman <packman150@hotmail.com>
2024-08-02 17:19:38 -0700

    img

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -3,7 +3,7 @@ import './Img.css';
 const Img = ({ img, alignment }) => {
     return (
         <div className={`ImgContainer ${alignment}`} >
-            <img src="small.jpg" srcset="small.jpg 600w, medium.jpg 1200w, large.jpg 1800w" alt="Responsive image"/>
+            <img src={img} srcset="small.jpg 600w, medium.jpg 1200w, large.jpg 1800w" alt="Responsive image"/>
         </div>
     );
 };
