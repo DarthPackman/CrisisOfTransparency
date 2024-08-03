77f26d160dab290c1550f9356726f8264526bb84
DarthPackman <packman150@hotmail.com>
2024-08-02 17:17:52 -0700

    img fix

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -3,7 +3,7 @@ import './Img.css';
 const Img = ({ img, alignment }) => {
     return (
         <div className={`ImgContainer ${alignment}`} >
-            <img src={img} alt="Image" className="ImgComponent" />
+            <img src="small.jpg" srcset="small.jpg 600w, medium.jpg 1200w, large.jpg 1800w" alt="Responsive image"/>
         </div>
     );
 };
