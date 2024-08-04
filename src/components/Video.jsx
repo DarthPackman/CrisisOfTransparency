0a1658a72dc98f626f8da01e852858bbba30a2d7
DarthPackman <packman150@hotmail.com>
2024-08-04 15:33:59 -0700

    video change

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,25 @@
-import './Video.css'
+import { useEffect, useRef } from 'react';
+import './Video.css';
 
 const Video = ({ videoPath, videoPath2, alignment }) => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.play().catch(error => {
+                console.error('Error trying to play the video:', error);
+            });
+        }
+    }, []);
+
     return (
-        <div className='VidContainer' >
-            <video autoPlay className={`video ${alignment}`} muted loop>
+        <div className='VidContainer'>
+            <video ref={videoRef} autoPlay className={`video ${alignment}`} muted loop>
                 <source src={videoPath} type="video/webm" />
                 <source src={videoPath2} type="video/mp4" />
             </video>
         </div>
-    )
+    );
 };
 
 export default Video;
