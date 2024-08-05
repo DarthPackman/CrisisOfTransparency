cd1d357d976459312ff8035751452a9a24b4d2fe
DarthPackman <packman150@hotmail.com>
2024-08-04 17:21:25 -0700

    fix link vuln

--- src/components/LinkedText.jsx
+++ src/components/LinkedText.jsx
@@ -7,7 +7,7 @@ const Text = ({ background, alignment, type, link1, link2, link3 }) => {
                 <h1 className={`TextStyle ${type}`}>
                     Portfolios
                     <p>
-                        <a href={`${link1}`} target={'_blank'}> Gavin</a> <a href={`${link2}`} target={'_blank'}> Carlos</a> <a href={`${link3}`} target={'_blank'}> Ian</a>
+                        <a href={`${link1}`} target={'_blank'} rel={'noopener noreferrer'}> Gavin</a> <a href={`${link2}`} target={'_blank'} rel={'noopener noreferrer'}> Carlos</a> <a href={`${link3}`} target={'_blank'} rel={'noopener noreferrer'}> Ian</a>
                     </p>
                 </h1>
             </div>
